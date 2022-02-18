47979e38deeaf1b3ee5fbc818179ad692fe7f098
Alejandra Santiso <alejadra.paidoslalin@gmail.com>
2022-02-18 12:47:54 +0100

    crear class

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 //las importaciones siempre van al principio del archivo
- import {saludar} from "./js/componentes.js";
- import './styles.css';
+ 
+ import { Todo } from './classes/todo.class';
+import './styles.css';
 
 //en los llaves se pone lo que se quiere llamar
 //si quieres llamar todo le pones ?
 
 
 
-const nombre= 'Zoé'
-
-saludar(nombre);
 
 
 
+
+const tarea=new Todo ('Aprender JavaScrip');
+
+console.log (tarea);
