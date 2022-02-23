206f33fedeaf04a25d66a7d6cf95ac09579179f0
Alejandra Santiso <alejadra.paidoslalin@gmail.com>
2022-02-23 13:55:57 +0100

    Operador Ternario

--- src/index.js
+++ src/index.js
@@ -6,11 +6,32 @@ import { crearTodoHtml } from './js/componentes';
 
 export const todoList=new TodoList();
 
-const tarea=new Todo('Aprender Javascript');
-
-todoList.nuevoTodo(tarea);
-
-console.log(todoList);
 
 
-crearTodoHtml(tarea);
+// const tarea=new Todo('Aprender Javascript');
+
+// todoList.nuevoTodo(tarea);
+
+// console.log(todoList);
+
+
+// crearTodoHtml(tarea);
+
+localStorage.setItem('mi-key','');
+localStorage.removeItem('mi-key');  
+
+
+
+
+//solo se puede almacenar string
+
+// Pone el setTimeout para visualizar que al pasar un tiemo se elimina 
+// la información
+
+
+// setTimeout(() => {
+    
+// }, timeout);
+
+
+
