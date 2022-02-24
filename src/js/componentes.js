9c17af4cc20770096ab2e6bed3c9ddc65bf4422e
Alejandra Santiso <alejadra.paidoslalin@gmail.com>
2022-02-24 13:41:00 +0100

    Subir

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -44,7 +44,7 @@ txtInput.addEventListener('keyup',(event)=>{
 divTodoList.addEventListener('click',(event)=>{
     const nombreElemento=event.target.localName;
     const todoElemento=event.target.parentElement.parentElement;
-    const todoId=todoElemento.getAtribute('data-id');
+    const todoId=todoElemento.getAttribute('data-id');
 
     if (nombreElemento.includes("input")){
         todoList.marcarCompletado(todoId);
